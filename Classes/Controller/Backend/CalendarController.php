4a8f051f158d2f803cfc910d978903a072264a18
Maik Schneider <maik.schneider@xima.de>
2026-01-23 10:21:30 +0100

    fix: use new pid in backend module

--- Classes/Controller/Backend/CalendarController.php
+++ Classes/Controller/Backend/CalendarController.php
@@ -9,7 +9,7 @@ class CalendarController extends AbstractBackendController
     public function getRecordPid(): int
     {
         // TODO: Make this configurable via extension configuration or TypoScript
-        return 1424;
+        return 1468;
     }
 
     public function getTableNames(): array
