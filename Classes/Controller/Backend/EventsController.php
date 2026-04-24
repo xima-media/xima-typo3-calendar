832ab0625fd66d2e07b0bf53ee2d6f994f99f877
Markus Hackel <mha@xima.de>
2026-04-24 15:43:21 +0200

    feat: add relation labels in rows for appointments, events, locations

--- Classes/Controller/Backend/EventsController.php
+++ Classes/Controller/Backend/EventsController.php
@@ -5,6 +5,7 @@ namespace Xima\XimaTypo3Calendar\Controller\Backend;
 use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationExtensionNotConfiguredException;
 use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationPathDoesNotExistException;
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
+use TYPO3\CMS\Core\Database\Connection;
 use Xima\XimaTypo3Recordlist\Controller\AbstractBackendController;
 
 class EventsController extends AbstractBackendController
@@ -47,6 +48,10 @@ class EventsController extends AbstractBackendController
                 $record['url'] = '/aktuelles/veranstaltungen/event/' . $record['uid'] . '-slug';
             }
         }
+
+        $this->addAppointmentsToEvents();
+        $this->addEventsToAppointments();
+        $this->addLocations();
     }
 
     protected function modifyTableConfiguration(): void
@@ -97,5 +102,70 @@ class EventsController extends AbstractBackendController
         // ============================================
         $this->tableConfiguration['tx_ximatypo3calendar_domain_model_requirement']['columns']['title']['defaultPosition'] = 1;
         $this->tableConfiguration['tx_ximatypo3calendar_domain_model_requirement']['columns']['assignee']['defaultPosition'] = 2;
+    }
+
+    private function addAppointmentsToEvents(): void
+    {
+        if ($this->getTableName() !== 'tx_ximatypo3calendar_domain_model_event') {
+            return;
+        }
+
+        foreach ($this->records as &$record) {
+            $qb = $this->connectionPool->getQueryBuilderForTable('tx_ximatypo3calendar_domain_model_entry');
+            $appointments = $qb->select('*')
+                ->from('tx_ximatypo3calendar_domain_model_entry')
+                ->where(
+                    $qb->expr()->eq('event', $qb->createNamedParameter($record['uid'], Connection::PARAM_INT)),
+                    $qb->expr()->eq('record_type', $qb->createNamedParameter('event-appointment'))
+                )
+                ->orderBy('start_date', 'ASC')
+                ->executeQuery()
+                ->fetchAllAssociative();
+
+            $record['appointments'] = implode(', ', array_map(static function ($appointment) {
+                $date = date_create($appointment['start_date']);
+                return $date ? date_format($date, 'd.m.Y H:i') : null;
+            }, $appointments));
+        }
+    }
+
+    private function addEventsToAppointments(): void
+    {
+        if ($this->getTableName() !== 'tx_ximatypo3calendar_domain_model_entry') {
+            return;
+        }
+
+        foreach ($this->records as &$record) {
+            $qb = $this->connectionPool->getQueryBuilderForTable('tx_ximatypo3calendar_domain_model_event');
+            $eventTitle = $qb->select('title')
+                ->from('tx_ximatypo3calendar_domain_model_event')
+                ->where(
+                    $qb->expr()->eq('uid', $qb->createNamedParameter($record['event'], Connection::PARAM_INT)),
+                )
+                ->executeQuery()
+                ->fetchOne();
+
+            $record['event'] = $eventTitle ?: '';
+        }
+    }
+
+    private function addLocations(): void
+    {
+        if (!in_array($this->getTableName(), ['tx_ximatypo3calendar_domain_model_event', 'tx_ximatypo3calendar_domain_model_entry'])) {
+            return;
+        }
+
+        foreach ($this->records as &$record) {
+            $qb = $this->connectionPool->getQueryBuilderForTable('tx_ximatypo3calendar_domain_model_location');
+            $locationName = $qb->select('name')
+                ->from('tx_ximatypo3calendar_domain_model_location')
+                ->where(
+                    $qb->expr()->eq('uid', $qb->createNamedParameter($record['location'], Connection::PARAM_INT)),
+                )
+                ->executeQuery()
+                ->fetchOne();
+
+            $record['location'] = $locationName ?: '';
+        }
     }
 }
