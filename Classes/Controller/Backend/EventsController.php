dd48b829dc2337a6deb213898c17cf09a4be041a
Markus Hackel <mha@xima.de>
2026-04-28 08:38:05 +0200

    feat: enable date filter for appointments in event module

--- Classes/Controller/Backend/EventsController.php
+++ Classes/Controller/Backend/EventsController.php
@@ -3,6 +3,7 @@
 namespace Xima\XimaTypo3Calendar\Controller\Backend;
 
 use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Types\Type;
 use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationExtensionNotConfiguredException;
 use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationPathDoesNotExistException;
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
@@ -68,6 +69,7 @@ class EventsController extends AbstractBackendController
         $this->tableConfiguration['tx_ximatypo3calendar_domain_model_event']['columns']['publish_to_website']['defaultPosition'] = 6;
         $this->tableConfiguration['tx_ximatypo3calendar_domain_model_event']['columns']['publish_date']['defaultPosition'] = 7;
         $this->tableConfiguration['tx_ximatypo3calendar_domain_model_event']['columns']['appointments']['defaultPosition'] = 8;
+        $this->tableConfiguration['tx_ximatypo3calendar_domain_model_event']['columns']['appointments']['filter']['partial'] = 'DateTime';
 
         $this->tableConfiguration['tx_ximatypo3calendar_domain_model_event']['columns']['location']['filter']['items'] = $this->getFilterItemsForTable('tx_ximatypo3calendar_domain_model_location');
 
@@ -107,6 +109,18 @@ class EventsController extends AbstractBackendController
         // ============================================
         $this->tableConfiguration['tx_ximatypo3calendar_domain_model_requirement']['columns']['title']['defaultPosition'] = 1;
         $this->tableConfiguration['tx_ximatypo3calendar_domain_model_requirement']['columns']['assignee']['defaultPosition'] = 2;
+    }
+
+    protected function addAdditionalConstraints(): void
+    {
+        $body = $this->request->getParsedBody();
+        if (is_array($body) && !empty($body['filter'])) {
+            foreach ($body['filter'] as $field => $data) {
+                if ($field === 'appointments' && !empty($data['value'])) {
+                    $this->addAppointmentsConstraint($data['value'], $data['expr'] ?? 'eq');
+                }
+            }
+        }
     }
 
     private function addAppointmentsToEvents(): void
@@ -200,5 +214,59 @@ class EventsController extends AbstractBackendController
         }
 
         return $items;
+    }
+
+    private function addAppointmentsConstraint(string $value, string $expr): void
+    {
+        if ($value === '') {
+            return;
+        }
+
+        foreach ($this->additionalConstraints as $key => $constraint) {
+            // remove existing appointment constraints as they do not respect the relation yet
+            if (str_contains($constraint, 'appointments')) {
+                unset($this->additionalConstraints[$key]);
+            }
+        }
+        $qb = $this->connectionPool->getQueryBuilderForTable('tx_ximatypo3calendar_domain_model_entry');
+        $qb->select('event')
+            ->distinct()
+            ->from('tx_ximatypo3calendar_domain_model_entry')
+            ->where(
+                $qb->expr()->eq(
+                    'record_type',
+                    $qb->createNamedParameter('event-appointment')
+                )
+            );
+        match ($expr) {
+            'lt' => $qb->andWhere($qb->expr()->lt(
+                'start_date',
+                $qb->createNamedParameter($value, Type::getType('datetime'))
+            )),
+            'gt' => $qb->andWhere($qb->expr()->gt(
+                'start_date',
+                $qb->createNamedParameter($value, Type::getType('datetime'))
+            )),
+            'neq' => $qb->andWhere($qb->expr()->neq(
+                'start_date',
+                $qb->createNamedParameter($value, Type::getType('datetime'))
+            )),
+            default => $qb->andWhere($qb->expr()->eq(
+                'start_date',
+                $qb->createNamedParameter($value, Type::getType('datetime'))
+            ))
+        };
+
+        $uids = $qb->executeQuery()->fetchAllNumeric();
+        // prepare for in constraint
+        $value = array_map('current', $uids);
+        if (empty($value)) {
+            $this->additionalConstraints[] = $this->queryBuilder->expr()->eq('t1.uid', 0);
+            return;
+        }
+        $this->additionalConstraints[] = $this->queryBuilder->expr()->in(
+            't1.uid',
+            $this->queryBuilder->createNamedParameter($value, Connection::PARAM_INT_ARRAY)
+        );
     }
 }
