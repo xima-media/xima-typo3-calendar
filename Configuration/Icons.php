545f977628f62e56c9b571c584b407216272acbb
Maik Schneider <maik.schneider@xima.de>
2026-06-29 14:51:50 +0200

    feat: add module group

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -1,6 +1,10 @@
 <?php
 
 return [
+    'module-group-calendar' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:xima_typo3_calendar/Resources/Public/Icons/module-group-calendar.svg',
+    ],
     'module-calendar-calendar' => [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
         'source' => 'EXT:xima_typo3_calendar/Resources/Public/Icons/module-calendar.svg',
