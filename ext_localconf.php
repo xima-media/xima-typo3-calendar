6c783a2f038c613208626ecf8ff5c79e7ec69e4f
Maik Schneider <maik.schneider@xima.de>
2026-07-09 11:01:11 +0200

    feat: make live events and appointments non-editable without proper permissions

--- ext_localconf.php
+++ ext_localconf.php
@@ -19,6 +19,14 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['proc
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['xima_typo3_calendar_owner']
     = \Xima\XimaTypo3Calendar\Hooks\EventOwnerBackendUserHook::class;
 
+// Render the edit form read-only for backend users who may not handle live
+// events when the opened record is a live event or an appointment of one.
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['formDataGroup']['tcaDatabaseRecord'][\Xima\XimaTypo3Calendar\Backend\FormDataProvider\LiveEventReadOnlyForNonPublisher::class] = [
+    'depends' => [
+        \TYPO3\CMS\Backend\Form\FormDataProvider\DatabaseEditRow::class,
+    ],
+];
+
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['xima_typo3_calendar2']
     = \Xima\XimaTypo3Calendar\Hooks\DataHandlerEventDispatcherHook::class;
 
